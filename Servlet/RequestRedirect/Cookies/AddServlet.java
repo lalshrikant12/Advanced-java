ef16bb7bc4ef220842608ee269c1b85de62672f6
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-04-23 23:45:31 +0530

    Create  AddServlet.java

---
+++ Servlet/RequestRedirect/Cookies/AddServlet.java
@@ -0,0 +1,33 @@
+package com.skl;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+public class AddServlet extends HttpServlet  
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
+	{
+		int i =Integer.parseInt(req.getParameter("num1"));
+		int j =Integer.parseInt(req.getParameter("num2"));
+		
+		int k=i+j;
+		//k=k*k;
+		
+		
+		
+		Cookie cookie = new Cookie("k",k + "");
+		res.addCookie(cookie); 
+		res.sendRedirect("sq");
+		
+		//req.setAttribute("k", k);
+		//RequestDispatcher rd = req.getRequestDispatcher("sq");
+		//rd.forward(req,res);
+		
+	}
+}
